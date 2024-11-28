2aa21619cb5fcb11ad8d4894733910a05297b869
czheisenberg <czheisenberg@qq.com>
2024-11-28 19:24:45 +0800

    feat:notice ok

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -19,6 +19,21 @@
 				</swiper-item>
 			</swiper>
 		</view>
+		
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="notification" size="20" color="#28b389"></uni-icons>
+				<text class="text">公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical="true" circular="true" autoplay="true" interval="1500" duration="300">
+					<swiper-item v-for="item in 5">具体内容内容内容内容内容内容内容内容内容内容内容</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="20" color="#333"></uni-icons>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -44,6 +59,46 @@
 					border-radius: 10rpx;
 				}
 			}
+		}
+	}
+	.notice{
+		width: 690rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+		background-color: #f9f9f9;
+		border-radius: 80rpx;
+		margin: 0 auto;
+		display: flex;
+		.left{
+			width: 140rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			text{
+				color: #28b389;
+				font-size: 28rpx;
+				font-weight: 600
+			}
+		}
+		.center{
+			flex: 1;
+			swiper{
+				height: 100%;
+				&-item{
+					height: 100%;
+					font-size: 30rpx;
+					color: #666;
+					overflow: hidden;
+					white-space: nowrap;
+					text-overflow: ellipsis; // ...
+				}
+			}
+		}
+		.right{
+			width: 70rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 		}
 	}
 }
