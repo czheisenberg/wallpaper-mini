96ea250b5982822c86ac89d2c07e1f6709aead71
czheisenberg <czheisenberg@qq.com>
2024-12-01 14:31:34 +0800

    feat: 用户中心

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,75 @@
 <template>
-	<view>
-		我的
+	<view class="userLayout">
+		<view class="userInfo">
+			<view class="avatar">
+				<image src="../../static/images/5.jpg" mode="aspectFill"></image>
+			</view>
+			<view class="nickname">鸡你太美</view>
+			<view class="ip">IP: 重庆</view>
+		</view>
+		
+		<view class="section">
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="cloud-download-filled" size="25" color="#28b389"></uni-icons>
+						<view class="text">我的下载</view>
+					</view>
+					<view class="right">
+						<view class="text">33</view>
+						<uni-icons type="right" size="20" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				
+				<view class="row">
+					<view class="left">
+						<uni-icons type="heart-filled" size="25" color="#28b389"></uni-icons>
+						<view class="text">我的评分</view>
+					</view>
+					<view class="right">
+						<view class="text">3</view>
+						<uni-icons type="right" size="20" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				
+				<view class="row">
+					<view class="left">
+						<uni-icons type="chatboxes-filled" size="25" color="#28b389"></uni-icons>
+						<view class="text">联系客服</view>
+					</view>
+					<view class="right">
+						<view class="text">0</view>
+						<uni-icons type="right" size="20" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="section">
+			<view class="list">			
+				<view class="row">
+					<view class="left">
+						<uni-icons type="notification-filled" size="25" color="#28b389"></uni-icons>
+						<view class="text">订阅更新</view>
+					</view>
+					<view class="right">
+						<view class="text"></view>
+						<uni-icons type="right" size="20" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				
+				<view class="row">
+					<view class="left">
+						<uni-icons type="flag-filled" size="25" color="#28b389"></uni-icons>
+						<view class="text">常见问题</view>
+					</view>
+					<view class="right">
+						<view class="text"></view>
+						<uni-icons type="right" size="20" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -9,6 +78,71 @@
 
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.userLayout{
+	.userInfo{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		padding: 50rpx 0;
+		
+		.avatar{
+			width: 160rpx;
+			height: 160rpx;
+			border-radius: 50%;
+			overflow: hidden;
+			image{
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.nickname{
+			font-size: 50rpx;
+			padding: 10rpx 0;
+			color: #333;
+		}
+		.ip{
+			color: #bbb;
+			font-size: 25rpx;
+		}
+	}
+		
+	.section{
+		width: 690rpx;
+		margin: 30rpx auto;
+		border: 1px solid #eee;
+		border-radius: 10rpx;
+		box-shadow: 0 0 30rpx rgba(0,0,0,0.05);
+		.list{
+			.row{
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				padding: 0 30rpx;
+				height: 100rpx;
+				border-bottom: 1px solid #eee;
+				&:last-child{
+					border-bottom: none;
+				}
+				.left{
+					display: flex;
+					align-items: center;
+					gap: 15rpx;
+					.text{
+						color: #666;
+					}
+				}
+				.right{
+					display: flex;
+					align-items: center;
+					.text{
+						font-size: 28rpx;
+						color: #aaa;
+					}
+				}
+			}
+		}
+	}
+}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/user/user.js
+++ unpackage/dist/dev/mp-weixin/pages/user/user.js
@@ -1,8 +1,69 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+const common_assets = require("../../common/assets.js");
 const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  _easycom_uni_icons2();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  _easycom_uni_icons();
+}
+function _sfc_render(_ctx, _cache) {
+  return {
+    a: common_assets._imports_0$1,
+    b: common_vendor.p({
+      type: "cloud-download-filled",
+      size: "25",
+      color: "#28b389"
+    }),
+    c: common_vendor.p({
+      type: "right",
+      size: "20",
+      color: "#aaa"
+    }),
+    d: common_vendor.p({
+      type: "heart-filled",
+      size: "25",
+      color: "#28b389"
+    }),
+    e: common_vendor.p({
+      type: "right",
+      size: "20",
+      color: "#aaa"
+    }),
+    f: common_vendor.p({
+      type: "chatboxes-filled",
+      size: "25",
+      color: "#28b389"
+    }),
+    g: common_vendor.p({
+      type: "right",
+      size: "20",
+      color: "#aaa"
+    }),
+    h: common_vendor.p({
+      type: "notification-filled",
+      size: "25",
+      color: "#28b389"
+    }),
+    i: common_vendor.p({
+      type: "right",
+      size: "20",
+      color: "#aaa"
+    }),
+    j: common_vendor.p({
+      type: "flag-filled",
+      size: "25",
+      color: "#28b389"
+    }),
+    k: common_vendor.p({
+      type: "right",
+      size: "20",
+      color: "#aaa"
+    })
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-0f7520f0"]]);
 wx.createPage(MiniProgramPage);
